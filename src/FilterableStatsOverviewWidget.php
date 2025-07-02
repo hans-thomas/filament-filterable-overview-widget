18b9271a05773ba83b8307e507618a351735ade9
Mohammad Mortazavi <s.mohammad027@live.com>
2025-07-02 19:09:23 +0330

    Create main widget class;

--- src/FilterableStatsOverviewWidget.php
+++ src/FilterableStatsOverviewWidget.php
@@ -2,4 +2,69 @@
 
 namespace FilterableStatsWithFormOverview\FilterableStatsOverviewWidget;
 
-class FilterableStatsOverviewWidget {}
+use Filament\Actions\Concerns\InteractsWithActions;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Form;
+use Filament\Pages\Concerns\InteractsWithFormActions;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+
+/**
+ * @property Form $form
+ */
+abstract class FilterableStatsOverviewWidget extends BaseWidget implements HasForms
+{
+    use InteractsWithActions;
+    use InteractsWithFormActions;
+    use InteractsWithForms;
+
+    protected static string $view = 'filterableStatsOverviewWidget::filterable-overview';
+
+    protected static ?string $pollingInterval = null;
+
+    protected ?string $item_label = null;
+
+    public ?int $item_id = null;
+
+    protected function getColumns(): int
+    {
+        return 4;
+    }
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema(
+                [
+                    Select::make('item_id')
+                        ->label($this->getItemLabel())
+                        ->lazy()
+                        ->afterStateUpdated(fn ($state) => $this->processFormData())
+                        ->searchable()
+                        ->getSearchResultsUsing($this->getSearchResultsUsing())
+                        ->getOptionLabelUsing($this->getOptionLabelUsing()),
+                ]
+            );
+    }
+
+    public function processFormData(): void
+    {
+        $this->form->validate();
+        $this->resetCachedStats();
+    }
+
+    protected function resetCachedStats(): void
+    {
+        $this->cachedStats = null;
+    }
+
+    abstract protected function getSearchResultsUsing(): callable;
+
+    abstract protected function getOptionLabelUsing(): callable;
+
+    protected function getItemLabel(): ?string
+    {
+        return $this->item_label;
+    }
+}
