f19974fc130729cf39aed070c2998d9f5018209b
hans-thomas <39920372+hans-thomas@users.noreply.github.com>
2025-07-02 10:39:06 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -12,11 +12,11 @@ use Filament\Notifications\NotificationsServiceProvider;
 use Filament\Support\SupportServiceProvider;
 use Filament\Tables\TablesServiceProvider;
 use Filament\Widgets\WidgetsServiceProvider;
+use FilterableStatsWithFormOverview\FilterableStatsOverviewWidget\FilterableStatsOverviewWidgetServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
-use FilterableStatsWithFormOverview\FilterableStatsOverviewWidget\FilterableStatsOverviewWidgetServiceProvider;
 
 class TestCase extends Orchestra
 {
