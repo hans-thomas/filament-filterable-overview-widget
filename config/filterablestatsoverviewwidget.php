0bd3d98255c2c78eed0ae09fddde3daf68da09cf
Mohammad Mortazavi <s.mohammad027@live.com>
2025-07-02 14:24:46 +0330

    Remove unneeded files;

--- config/filterablestatsoverviewwidget.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// config for FilterableStatsWithFormOverview/FilterableStatsOverviewWidget
-return [
-
-];
--- resources/lang/en/filterablestatsoverviewwidget.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// translations for FilterableStatsWithFormOverview/FilterableStatsOverviewWidget
-return [
-    //
-];
